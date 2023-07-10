24ae17633b2198c51ba73ec2ee182d4221edddd5
Claudineia Bueno <137913421+NBueno22@users.noreply.github.com>
2023-07-10 15:27:06 -0400

    Alterando code

--- readme.js
+++ readme.js
@@ -1,1 +1,11 @@
+# Sistema de cadastro de jogos
+
+> Status do projeto: Em desenvolvimento
+
+Para rodar esse projeto na sua máquina, por favor digite
+
+```
+node app.js
+```
+
 console.log("Rodando o sistema de gerenciamento de jogos")
